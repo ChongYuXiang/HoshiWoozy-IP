72626e89c473c48840827356029cb3809cee32bf
ChongYuXiang <s10257151@connect.np.edu.sg>
2024-02-01 03:46:28 +0800

    Updates Section + Scroll Function

---
+++ script.js
@@ -0,0 +1,16 @@
+function navFunction1() {
+    const element = document.getElementById("info");
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function navFunction2() {
+    const element = document.getElementById("characters");
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function navFunction3() {
+    const element = document.getElementById("updates");
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function navFunction4() {
+    const element = document.getElementById("shop");
+    element.scrollIntoView({ behavior: "smooth" });
+}
