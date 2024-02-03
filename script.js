1c494adc2a1cfcf1106ef4ae514703e281961a72
ChongYuXiang <s10257151@connect.np.edu.sg>
2024-02-03 20:46:53 +0800

    API added

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ function navFunction4() {
     const element = document.getElementById("shop");
     element.scrollIntoView({ behavior: "smooth" });
 }
+
+let url = 'https://randomuser.me/api/';
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    const el = document.getElementById('signIn');
+    if (el) {
+        el.addEventListener('click', function(){
+            fetch(url)
+            .then(response => response.json()) 
+            .then(function(data){
+            console.log(data)
+            console.log(data.results[0].email)
+            let image = document.getElementById("avatar");
+            image.src = data.results[0].picture.thumbnail;
+            let userTxt = document.getElementById("username");
+            userTxt.textContent = data.results[0].login.username;
+            let emailTxt = document.getElementById("email");
+            emailTxt.textContent = data.results[0].email;
+            });
+            let points = document.getElementById("points");
+            points.textContent = "5000"
+        });
+    }
+});
